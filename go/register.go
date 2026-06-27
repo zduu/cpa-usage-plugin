b58101d0c40057346c4172a0be1a7642a72262a3
zduu <mail@edxx.de>
2026-06-27 13:06:40 +0800

    Bump version to 1.2.14

--- go/register.go
+++ go/register.go
@@ -9,7 +9,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-const pluginVersion = "1.2.13"
+const pluginVersion = "1.2.14"
 
 func handleRegister(requestBody []byte) ([]byte, error) {
 	applyRuntimeConfig(requestBody)
