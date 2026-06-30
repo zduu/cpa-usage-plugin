caf8c40a7a8ff1336f5dd1bad9d2f46457efa576
zduu <mail@edxx.de>
2026-06-30 10:57:21 +0800

    chore(release): 发布 v2.1.0

--- go/register.go
+++ go/register.go
@@ -9,7 +9,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-const pluginVersion = "2.0.2"
+const pluginVersion = "2.1.0"
 
 func handleRegister(requestBody []byte) ([]byte, error) {
 	applyRuntimeConfig(requestBody)
