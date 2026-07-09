3485415d2ed48e96c3bb8009d181e45293b6d8ea
zduu <mail@edxx.de>
2026-07-10 01:55:36 +0800

    chore: 发布 v2.3.1
    Dashboard 视觉优化版本

--- go/register.go
+++ go/register.go
@@ -9,7 +9,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-const pluginVersion = "2.3.0"
+const pluginVersion = "2.3.1"
 
 func handleRegister(requestBody []byte) ([]byte, error) {
 	applyRuntimeConfig(requestBody)
