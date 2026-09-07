9cff1c7da5982a6b4d94c36896a5f9376dfdb4d1
zduu <mail@edxx.de>
2026-09-07 16:04:48 +0800

    chore(release): 发布 v2.6.4

--- go/register.go
+++ go/register.go
@@ -12,7 +12,7 @@ import (
 
 const pluginID = "usage-dashboard-zduu"
 const legacyPluginID = "usage-statistics"
-const pluginVersion = "2.6.3"
+const pluginVersion = "2.6.4"
 
 func handleRegister(requestBody []byte) ([]byte, error) {
 	applyRuntimeConfig(requestBody)
